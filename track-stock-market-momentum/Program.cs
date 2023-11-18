617474121b1198a3d0218aac1f11985675d846c4
CalGrimes <cal.grimes@outlook.com>
2023-11-18 13:15:06 +0000

    Update program.cs

--- track-stock-market-momentum/Program.cs
+++ track-stock-market-momentum/Program.cs
@@ -15,13 +15,8 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllers();
 
 app.Run();
