378a5544bee1a5d55b67561faaf0faad571f541d
CalGrimes <cal.grimes@outlook.com>
2023-11-18 13:12:57 +0000

    Change from razor pages to MVC

---
+++ track-stock-market-momentum/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace track_stock_market_momentum.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
