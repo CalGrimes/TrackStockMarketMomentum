cf5de27d9ada535865f845ef15da2076f916bf98
CalGrimes <cal.grimes@outlook.com>
2023-11-19 16:36:24 +0000

    Finnhub Service + Frontend Hero

---
+++ track-stock-market-momentum/TradingOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace track_stock_market_momentum
+{
+ /// <summary>
+ /// Represents Options pattern for "StockPrice" configuration
+ /// </summary>
+ public class TradingOptions
+ {
+  public string? DefaultStockSymbol { get; set; }
+ }
+}
